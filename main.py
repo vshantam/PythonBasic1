6f5b49c906e63511dbcc42db527a9cc3fcb87662
oleksandrse <sanie4eg@gmail.com>
2015-04-25 00:30:41 +0300

    issues 1-4, added literals module, rewritten 2 issue, proposed implementation for issue 4.

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 # coding: utf8
-print "Привет"
-
+from literals import welcome_greeting  # importing all literals, however <from literals import *> might be better
+from literals import cells_number
+from literals import current_cell
 from random import randint
-print randint(1,6)
+
+print welcome_greeting  # printing greeting
+
+print randint(1, 6)
 
 a = raw_input('Player name: ')
+
+
 def name(a):
-   return a
+    return a
+
+
 print a
+
+
+# for issue 2 #
+def throw_dice():
+    return randint(1, 6)
+
+
+thrown_number = throw_dice()
+
+
+# for issue 4 #
+def moving(current_cell, thrown_number):
+    current_cell += thrown_number  # updates current position
+    if current_cell > cells_number:
+        current_cell -= cells_number  # if new position is over cells_number updates it to difference
+    return current_cell  # between them, and return new position
