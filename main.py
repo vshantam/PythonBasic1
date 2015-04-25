01fa8271f16fdf49b58a79e072167a673390ed2e
Nativeborn <nativeborncitizen@gmail.com>
2015-04-25 10:46:53 +0300

    Review

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
 # coding: utf8
-from literals import welcome_greeting  # importing all literals, however <from literals import *> might be better
-from literals import cells_number
-from literals import current_cell
+import settings
 from random import randint
 
-print welcome_greeting  # printing greeting
 
-a = raw_input('Player name: ')
-def name(a):
-    return a
-print a
+def print_greeting():
+    print settings.welcome_greeting
 
 
-# for issue 2 #
+def input_player_name():
+    return raw_input('Player name: ')
+
+
 def throw_dice():
     return randint(1, 6)
-print throw_dice()
-
-thrown_number = throw_dice()
 
 
-# for issue 4 #
-def moving(current_cell, thrown_number):
+def get_new_player_position(current_cell, thrown_number):
     """
     I think we should start counting the cell with 1 rather than with 0, or change the max number of cells to 43.
     This test brings the position after the initial throw.
-    >>> moving(0;2)
+    >>> get_new_player_position(0, 2)
     2
     This test estimates the position after crossing the start field.
-    >>> moving(43;4)
+    >>> get_new_player_position(43, 4)
     2
     """
-    current_cell += thrown_number  # updates current position
-    if current_cell > cells_number:
-        current_cell -= cells_number  # if new position is over cells_number updates it to difference
-    return current_cell  # between them, and return new position
+    return (current_cell + thrown_number) % settings.cells_number
+
+
+print_greeting()
+
+c = get_new_player_position(0, throw_dice())
---
+++ settings.py
@@ -0,0 +1,16 @@
+# coding: utf8
+"""
+This module include all literals for
+Monopoly project.
+Each literal (variable) should be followed
+with short and proper commentary
+"""
+
+# number of cells on the playing map
+cells_number = 44
+
+# welcome greeting, shown at the start of game
+welcome_greeting = 'Hello! Welcome to Monopoly!'
+
+# initial cell number
+initial_cell = 0
