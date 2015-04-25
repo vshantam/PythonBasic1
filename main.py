35572a9bab6cd6b19c3ee11e7863d64db28a3e51
pavlodatsiuk <pavlo.datsiuk@outlook.com>
2015-04-25 09:20:14 +0300

    added position test for the initial throw added position test for the "crossing the start field" situation

--- main.py
+++ main.py
@@ -6,28 +6,31 @@ from random import randint
 
 print welcome_greeting  # printing greeting
 
-print randint(1, 6)
-
 a = raw_input('Player name: ')
-
-
 def name(a):
     return a
-
-
 print a
 
 
 # for issue 2 #
 def throw_dice():
     return randint(1, 6)
-
+print throw_dice()
 
 thrown_number = throw_dice()
 
 
 # for issue 4 #
 def moving(current_cell, thrown_number):
+    """
+    I think we should start counting the cell with 1 rather than with 0, or change the max number of cells to 43.
+    This test brings the position after the initial throw.
+    >>> moving(0;2)
+    2
+    This test estimates the position after crossing the start field.
+    >>> moving(43;4)
+    2
+    """
     current_cell += thrown_number  # updates current position
     if current_cell > cells_number:
         current_cell -= cells_number  # if new position is over cells_number updates it to difference
